34ddaf46e200093ddd5dd5363d8f756fc3c7b7c2
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-09-17 21:48:12 +1200

    adapting responsiveness

--- app/Title/Title.tsx
+++ app/Title/Title.tsx
@@ -4,26 +4,24 @@ import Image from 'next/image'
 const Title = () => {
   return (
     <div className="home-background flex flex-col justify-between px-8 pt-52 pb-8">
-      <div className="pt-2 mb-4">
-        <h1 className="text-lg md:text-xl lg:text-2xl text-left mb-2.5">
-          SEA IT ALL
-        </h1>
-        <h3 className="text-4xl md:text-5xl lg:text-6xl text-left leading-tight mb-2.5">
-          MULTIBEAM SONAR <br className="mb-2" /> PRODUCTS & SOLUTIONS.
+      <div className="mt-2 mb-4">
+        <h1 className="text-2xl text-left mb-1">SEA IT ALL</h1>
+        <h3
+          className="text-6xl text-left mb-2.5 leading-tight"
+          style={{ width: '600px' }}
+        >
+          MULTIBEAM SONAR PRODUCTS & SOLUTIONS.
         </h3>
       </div>
-      <div className="text-center pt-52 md:text-left md:mt-1 mb-4 md:ml-96 relative">
+      <div className="text-center pt-44 md:text-left md:mt-7 mb-4 md:ml-96 relative">
         <div className="ml-14">
-          <div className="text-lg leading-tight">
-            <p>
-              WASSP multi-beam enables
-              <br />
-              sustainable fishing,
-              <br />
-              exploration, and surveys with
-              <br />
-              versatility and detail
-            </p>
+          <div className="leading-tight">
+            <div style={{ width: '220px', fontSize: '16px' }}>
+              <p>
+                WASSP multi-beam enables sustainable fishing, exploration, and
+                surveys with versatility and detail.
+              </p>
+            </div>
           </div>
         </div>
         <div className="absolute bottom-2 right-10">
--- app/Welcome/Welcome.tsx
+++ app/Welcome/Welcome.tsx
@@ -1,33 +1,51 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Welcome = () => {
   return (
     <main className="welcome-background">
       <div className="overlay">
-        <div className="flex flex-col justify-between px-8 pt-72 pb-52 ml-96">
-          <div className="my-1 ml-6">
-            <h1 className="lg:text-6xl text-left leading-tight ml-10 pl-18 mb-2.5">
-              WELCOME TO THE WORLD OF <br /> WASSP MULTIBEAM
+        <div className="flex flex-col ml-1 md:ml-72 lg:ml-96 px-0 md:px-1 lg:px-8 pt-56 pb-52">
+          <div className="lg:my-1 lg:ml-6">
+            <h1
+              className="text-6xl text-left leading-tight ml-10 pl-18 mb-2.5"
+              style={{ maxWidth: '700px' }}
+            >
+              WELCOME TO THE WORLD OF WASSP MULTIBEAM
             </h1>
-            <div className="text-center pl-10 pt-8 pb-18 md:text-left md:mt-1 mb-4 md:ml-18 relative">
+            <div className="text-left pl-10 pt-8 pb-18 mt-1 mb-4 md:ml-18 relative">
               <div className="text-base leading-tight flex space-x-24">
                 <p className="text-sm p-norwester">01</p>
-                <p className="text-sm p-norwester">ABOUT WASSP</p>
-                <p>
-                  WASSP, a powerful multi-beam sounder with
-                  <br />
-                  120-degree coverage and models for deep
-                  <br />
-                  exploration, offers unparalleled accuracy, detail,
-                  <br />
-                  and versatility. Target fish with steerable
-                  <br />
-                  sounders, generate real-time 3D charts, map fish
-                  <br />
-                  in water, and make better decisions with the
-                  <br />
-                  user-friendly advanced mode.
+                <p
+                  className="text-sm p-norwester"
+                  style={{ marginRight: '1rem' }}
+                >
+                  ABOUT WASSP
                 </p>
+                <div style={{ width: '365px' }}>
+                  <p>
+                    WASSP, a powerful multi-beam sounder with 120-degree
+                    coverage and models for deep exploration, offers
+                    unparalleled accuracy, detail, and versatility. Target fish
+                    with steerable sounders, generate real-time 3D charts, map
+                    fish in water, and make better decisions with the
+                    user-friendly advanced mode.
+                  </p>
+                </div>
+              </div>
+              <div className="absolute bottom-2 top-52 right-96 pr-2">
+                <a
+                  href="#contact"
+                  className="text-white bg-transparent py-3 px-8 rounded-full border border-white hover:bg-white hover:text-black flex items-center space-x-2"
+                >
+                  <span>Our Story</span>
+                  <Image
+                    src="/ArrowRight.svg"
+                    alt="Arrow Right"
+                    width={25}
+                    height={25}
+                  />
+                </a>
               </div>
             </div>
           </div>
