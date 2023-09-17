5fdb2ee8f24c8b2749c63c39aa2d1d2e201888a7
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-09-17 17:31:35 +1200

    update welcome component

--- app/Welcome/Welcome.tsx
+++ app/Welcome/Welcome.tsx
@@ -3,12 +3,33 @@ import React from 'react'
 const Welcome = () => {
   return (
     <main className="welcome-background">
-      <div className="gradient-layer">
-        <div className="flex flex-col justify-between px-8 py-52">
-          <div className="mb-4">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl text-left leading-tight mb-2.5">
+      <div className="overlay">
+        <div className="flex flex-col justify-between px-8 pt-72 pb-52 ml-96">
+          <div className="my-1 ml-6">
+            <h1 className="lg:text-6xl text-left leading-tight ml-10 pl-18 mb-2.5">
               WELCOME TO THE WORLD OF <br /> WASSP MULTIBEAM
             </h1>
+            <div className="text-center pl-10 pt-8 pb-18 md:text-left md:mt-1 mb-4 md:ml-18 relative">
+              <div className="text-base leading-tight flex space-x-24">
+                <p className="text-sm p-norwester">01</p>
+                <p className="text-sm p-norwester">ABOUT WASSP</p>
+                <p>
+                  WASSP, a powerful multi-beam sounder with
+                  <br />
+                  120-degree coverage and models for deep
+                  <br />
+                  exploration, offers unparalleled accuracy, detail,
+                  <br />
+                  and versatility. Target fish with steerable
+                  <br />
+                  sounders, generate real-time 3D charts, map fish
+                  <br />
+                  in water, and make better decisions with the
+                  <br />
+                  user-friendly advanced mode.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
