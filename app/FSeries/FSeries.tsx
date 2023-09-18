c58aa6cb2551049688ffda06d1e21082ddac8676
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-09-19 11:52:21 +1200

    fix responsiveness for FSeries component

--- app/FSeries/FSeries.tsx
+++ app/FSeries/FSeries.tsx
@@ -5,13 +5,18 @@ const FSeries = () => {
   return (
     <main className="series-background">
       <div className="md:pt-96"></div>
-      <div className="md:flex md:space-x-96 pt-6 md:pt-96 md:pb-6">
+      <div className="md:flex md:justify-between pt-6 md:pt-96 md:pb-6">
         <div className="ml-4 md:ml-9 space-y-4">
           <div className="mb-6">
             <LoadingBar />
           </div>
           <h1 className="text-xs hidden md:inline">(FISHING SERIES)</h1>
-          <h2 className="text-5xl lg:text-8xl lg:tracking-wide">F - SERIES</h2>
+          <h2
+            className="text-5xl lg:text-8xl lg:tracking-wide"
+            style={{ whiteSpace: 'nowrap', overflow: 'hidden' }}
+          >
+            F - SERIES
+          </h2>
         </div>
 
         <div className="ml-4 md:ml-9 space-y-4 lg:pl-36 md:mt-6">
