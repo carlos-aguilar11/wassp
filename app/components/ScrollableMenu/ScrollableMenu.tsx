a0f97bbdd73b93b801ebd751fd170d89f08181ba
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-09-20 18:54:51 +1200

    create missng svg logo and database for new menu

--- app/components/ScrollableMenu/ScrollableMenu.tsx
+++ app/components/ScrollableMenu/ScrollableMenu.tsx
@@ -1,48 +1,9 @@
 'use client'
 import React, { useState } from 'react'
-import data, { featureExplanations } from '../Menu/data'
+import data, { featureExplanations } from './menudata'
 
-const ScrollableFeatureMenu = () => {
-  const [buttonClicked, setButtonClicked] = useState<number | null>(null)
-
-  const handleButtonClick = (buttonNumber: number) => {
-    setButtonClicked(buttonNumber)
-  }
-
-  const formatNumber = (num: number) => (num < 10 ? `0${num}` : num)
-
-  return (
-    <div className="scrollable-menu">
-      <div className="menu-buttons">
-        {data.map((item: featureExplanations) => (
-          <button
-            key={item.id}
-            onClick={() => handleButtonClick(item.number)}
-            className={`menu-button ${
-              buttonClicked === item.number ? 'active-button' : ''
-            }`}
-          >
-            <span className="p-norwester">{formatNumber(item.number)}</span>
-            <span className="p-neue">{item.title}</span>
-          </button>
-        ))}
-      </div>
-
-      <div className="explanation">
-        {buttonClicked !== null && (
-          <div className="explanation-content">
-            <p className="number">{formatNumber(buttonClicked)}</p>
-            <p className="title">
-              {data.find((item) => item.number === buttonClicked)?.title}
-            </p>
-            <p className="explanation">
-              {data.find((item) => item.number === buttonClicked)?.explanation}
-            </p>
-          </div>
-        )}
-      </div>
-    </div>
-  )
+const ScrollableMenu = () => {
+  return <div></div>
 }
 
-export default ScrollableFeatureMenu
+export default ScrollableMenu
---
+++ app/components/ScrollableMenu/menudata.ts
@@ -0,0 +1,35 @@
+'use-client'
+export interface featureExplanations {
+  id: number
+  name: string
+  logo: string
+}
+
+const menudata = [
+  {
+    id: 1,
+
+    name: 'SBG SYSTEMS',
+    logo: '/SbgSystems.svg',
+  },
+  {
+    id: 2,
+
+    name: 'GPA SEABOTS',
+    logo: '/Seabots.svg',
+  },
+  {
+    id: 3,
+
+    name: 'HEMISPHERE',
+    logo: '/Hemisphere.svg',
+  },
+  {
+    id: 4,
+
+    name: 'FURUNO',
+    logo: '/Furuno.svg',
+  },
+]
+
+export default menudata
