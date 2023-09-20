5de7eed7b3d7c3241d8de4a16e45219120fc0c47
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-09-21 00:06:46 +1200

    update background testimonial component

--- app/components/Testimonial/Testimonial.tsx
+++ app/components/Testimonial/Testimonial.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Testimonial = () => {
   return (
     <main className="testimonial-background">
+      <div className="pt-48"></div>
       <h1 className="pt-72">Testimonial</h1>
       <div className="pt-48"></div>
     </main>
